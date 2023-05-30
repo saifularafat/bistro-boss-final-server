a4ee70caeade01d54cd5eefc6b8241a1c1b7b567
saifularafat <saifularafat.info@gmail.com>
2023-05-31 03:03:33 +0600

    users site get post patch and delete add

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
 // middleware
@@ -26,6 +27,84 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const userCollection = client.db('bistroDB').collection('users');
+        const menuCollection = client.db('bistroDB').collection('menu');
+        const reviewCollection = client.db('bistroDB').collection('reviews');
+        const cartCollection = client.db('bistroDB').collection('carts');
+
+        /* User related create Apis  */
+
+        app.get('/user', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result)
+        })
+
+        app.post('/user', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+            const existing = await userCollection.findOne(query);
+            if (existing) {
+                return res.send({ message: 'user already exists' })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result)
+        })
+
+        app.patch('/user/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        })
+
+        app.delete('/user/:id', async (req, res ) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id)};
+            const result = await userCollection.deleteOne(query);
+            res.send( result );
+        })
+
+        /* menu all data  apis */
+        app.get('/menu', async (req, res) => {
+            const result = await menuCollection.find().toArray();
+            res.send(result);
+        })
+
+        /* reviews all data apis */
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find().toArray();
+            res.send(result);
+        })
+
+
+        /* cart insert collation */
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            if (!email) {
+                res.send([]);
+            }
+            const query = { email: email };
+            const result = await cartCollection.find(query).toArray();
+            res.send(result)
+
+        })
+        app.post('/carts', async (req, res) => {
+            const item = req.body;
+            // console.log(cart);
+            const result = await cartCollection.insertOne(item);
+            res.send(result);
+        })
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query);
+            res.send(result);
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
@@ -45,3 +124,17 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Bistro Boss is sitting on port ${port}`);
 })
+
+
+/**
+ * --------------------------------
+ *      NAMING CONVENTION
+ * --------------------------------
+ * users : userCollection
+ * app.get('/users')
+ * app.get('/users/:id')
+ * app.post('/users')
+ * app.patch('/users/:id')
+ * app.put('/users/:id')
+ * app.delete('/users/:id')
+*/
