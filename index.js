ef328946399c84a67b15f3847db6951fea2d749e
saifularafat <saifularafat.info@gmail.com>
2023-06-01 02:26:14 +0600

    admin server site data secure

--- index.js
+++ index.js
@@ -10,6 +10,25 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+
+    /* bearer token */
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' });
+        }
+
+        req.decoded = decoded;
+        next();
+    })
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.guqonkt.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -32,24 +51,70 @@ async function run() {
         const reviewCollection = client.db('bistroDB').collection('reviews');
         const cartCollection = client.db('bistroDB').collection('carts');
 
-        /* User related create Apis  */
+        /* json web Token */
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token })
+        })
 
-        app.get('/user', async (req, res) => {
+        // ? warning :>  use verifyJWT before using verifyAdmin
+        const verifyAdmin = async ( req, res, next ) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne( query );
+            if( user?.role !== 'admin'){
+                return res.status(403).send({error: true , message: 'forbidden message'});
+            }
+            next();
+        }
+
+        /**
+         * 0. Do not show secure links to those who should not see the links
+         * 1. use jwt token: verifyJWT
+         * 2. use verify Admin middleware (mongodb connection use)
+         * 
+        */
+
+        /* User related create Apis  */
+        app.get('/user', verifyJWT, verifyAdmin, async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result)
         })
-
+        /* new user create Apis  */
         app.post('/user', async (req, res) => {
             const user = req.body;
             const query = { email: user.email };
             const existing = await userCollection.findOne(query);
+
             if (existing) {
                 return res.send({ message: 'user already exists' })
             }
+
             const result = await userCollection.insertOne(user);
             res.send(result)
         })
 
+        /* check the admin for web site */
+        /**
+         * 1st: security verify layOut one verifyJWT
+         * 2nd: security same Email match
+         * 3rd: Check the admin  Client site
+        */
+        app.get('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            // 2nd match email
+            if (req.decoded.email !== email) {
+                res.send({ admin: false })
+            }
+
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const result = { admin: user?.role === 'admin' }
+            res.send(result);
+        })
+
+        /* admin change the user Apis  */
         app.patch('/user/admin/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
@@ -62,11 +127,12 @@ async function run() {
             res.send(result)
         })
 
-        app.delete('/user/:id', async (req, res ) => {
+        /* admin panel delete the admin Apis  */
+        app.delete('/user/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await userCollection.deleteOne(query);
-            res.send( result );
+            res.send(result);
         })
 
         /* menu all data  apis */
@@ -83,11 +149,19 @@ async function run() {
 
 
         /* cart insert collation */
-        app.get('/carts', async (req, res) => {
+        app.get('/carts', verifyJWT, async (req, res) => {
             const email = req.query.email;
+
             if (!email) {
                 res.send([]);
             }
+
+            const decodedEmail = req.decoded.email;
+            console.log(decodedEmail);
+            if (email !== decodedEmail) {
+                return res.status(403).send({ error: true, message: 'provident access' })
+            }
+
             const query = { email: email };
             const result = await cartCollection.find(query).toArray();
             res.send(result)
